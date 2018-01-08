133b9d77414dbaeed58206c2370933da9461e3c2
GoergeCaoJ <cjooo092@gmail.com>
2018-01-08 12:26:27 +0800

    创建

---
+++ SpringNettyStudydemo/src/main/java/com/vic/SpringNettyStudydemo/App.java
@@ -0,0 +1,13 @@
+package com.vic.SpringNettyStudydemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
