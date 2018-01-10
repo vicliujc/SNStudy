31ef8df2c2173287c9ef08984c2b57f9896c98d5
GoergeCaoJ <cjooo092@gmail.com>
2018-01-10 16:07:46 +0800

    2018-01-10

---
+++ SpringNetty/src/main/java/com/vic/bean/CollectionBean.java
@@ -0,0 +1,21 @@
+package com.vic.bean;
+
+public class CollectionBean {
+	private String name;
+	private String[] empName;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String[] getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String[] empName) {
+		this.empName = empName;
+	}
+	
+
+}
